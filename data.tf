eb1e1d0003a4ccefb8ac2ed70d7bb17a2d735572
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-06 12:16:22 +0100

    fixing pipeline

--- data.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_api_management_product" "apim_product" {
-  count               = local.deploy_apim
-  product_id          = "${var.product}-product-${local.env}"
-  resource_group_name = local.apim_rg
-  api_management_name = local.apim_name
-}
