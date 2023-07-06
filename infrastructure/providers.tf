e80bdd82204749356517ad8acbdc0948dc27dd66
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-06 11:13:45 +0100

    fix pipeline

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = ">= 1.0.4"
+  backend "azurerm" {}
+
   required_providers {
     azurerm = {
+      source  = "hashicorp/azurerm"
       version = "3.41.0"
     }
+    random = {
+      source = "hashicorp/random"
+    }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
