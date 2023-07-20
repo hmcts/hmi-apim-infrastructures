87f8901ca81e96249e4413154132f56571960e07
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-20 17:28:28 +0100

    fixing error

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -7,15 +7,15 @@ data "policy_file" "api_operation_template" {
   remap_entries = [
       for policy in local.policies : {
 
-        operation_id = each.value.operationId
-        xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${each.value.templateFile}"),
+        operation_id = policy.operationId
+        xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${policy.templateFile}"),
           "#keyVaultHost#", var.key_vault_host),
           "#pihHost#", var.pih_host),
           "#snowHost#", var.snow_host)
-        display_name = each.value.display_name
-        method       = each.value.method
-        url_template = each.value.url_template
-        description  = each.value.description
+        display_name = policy.display_name
+        method       = policy.method
+        url_template = policy.url_template
+        description  = policy.description
       }
     ]
 }
