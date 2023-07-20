19cdfa3fe396d8f48d48c38a45b6da137829f0a7
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-20 17:13:32 +0100

    fixing build

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -1,3 +1,19 @@
 locals {
   policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
+  policies = [for policy in local.policies_data.policies : policy]
+}
+
+resource "policy_file" "api_operation_template" {
+  for_each = {
+    for policy in local.policies : policy.operationId => policy
+  }
+  operation_id = each.value.operationId
+  xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${each.value.templateFile}"),
+    "#keyVaultHost#", var.key_vault_host),
+    "#pihHost#", var.pih_host),
+    "#snowHost#", var.snow_host)
+  display_name = each.value.display_name
+  method       = each.value.method
+  url_template = each.value.url_template
+  description  = each.value.description
 }
