d73758838d01bdbb8449a67253cd8c02ecce2fe5
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-20 16:57:14 +0100

    fixing build

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -1,9 +1,3 @@
 locals {
   policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
-  remap_entries = [
-  ]
-}
-
-data "policy_file" "api_operation_template" {
-  TO_BE_REPLACED = local.remap_entries
 }
