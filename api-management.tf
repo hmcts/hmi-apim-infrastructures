4d40af81076ab359f528fa3ab8e9efa1f3b4d20c
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-20 16:35:48 +0100

    fixing build

--- api-management.tf
+++ api-management.tf
@@ -1,26 +1,6 @@
 locals {
   api_resource_group = "ss-${var.env}-network-rg"
   api-mgmt-prod-name = "${var.product}-product-${var.env}"
-  policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
-  remap_entries = [
-    for p in local.policies_data.policies : {
-      operation_id = p.operationId
-      xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${p.templateFile}"),
-        "#keyVaultHost#", var.key_vault_host),
-        "#pihHost#", var.pih_host),
-        "#snowHost#", var.snow_host)
-      display_name = p.display_name
-      method       = p.method
-      url_template = p.url_template
-      description  = each.value.description
-    }
-  ]
-}
-
-data "policy_file" "api_operation_template" {
-  vars = {
-    TO_BE_REPLACED = jsonencode(local.remap_entries)
-  }
 }
 
 module "apim_apis" {
@@ -38,7 +18,7 @@ module "apim_apis" {
   api_content_value         = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/future-hearings-hmi-api.json"
 
   policy_xml_content = file("${path.module}/resources/policy-files/api-policy.xml")
-  api_operations = data.policy_file.api_operation_template.vars.TO_BE_REPLACED
+  api_operations = data.policy_file.api_operation_template.TO_BE_REPLACED
 
   depends_on = [
     module.api_mgmt_product
