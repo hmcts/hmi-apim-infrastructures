441e67516b051b3703f61ca1c19882150d7d50c3
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 11:19:08 +0100

    add more content to template

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -6,6 +6,10 @@ output "policy_template_text" {
   value = [for policy in local.policies_data.policies :
     {
       operation_id = policy.operationId
+      display_name = policy.display_name
+      method       = policy.method
+      url_template = policy.url_template
+      description  = policy.description
     }
   ]
 }
