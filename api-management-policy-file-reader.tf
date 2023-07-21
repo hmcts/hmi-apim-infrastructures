6e5035758b956c5e6c9fd85e93511ac7acec6a35
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 17:20:25 +0100

    test

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -12,14 +12,7 @@ locals {
       method       = policy.method
       url_template = policy.url_template
       description  = policy.description
-      headers = [for header in policy.headers :
-        {
-          name = header.name
-          required = header.required
-          type = header.type
-          default_value = header.default_value
-        }
-      ]
+      headers = "test"
     }
   ]
 }
