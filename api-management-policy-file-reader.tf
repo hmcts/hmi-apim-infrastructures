e3a70ff060b3222141f2c1e229aea9ca5c2c6889
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 11:17:29 +0100

    populate tamplate

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -1,3 +1,11 @@
 locals {
   policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
 }
+
+output "policy_template_text" {
+  value = [for policy in local.policies_data.policies :
+    {
+      operation_id = policy.operationId
+    }
+  ]
+}
