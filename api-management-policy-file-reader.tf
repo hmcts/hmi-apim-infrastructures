f518c7388003529ef5d46ebed31cb2bef3b89f58
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 10:49:43 +0100

    revert changes

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -1,20 +1,6 @@
 locals {
   policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
-
-  remap_entries = flatten(
-    [for policy in local.policies_data.policies :
-      {
-        "operation_id" = policy.operationId
-        "xml_content"  = replace(replace(replace(file("${path.module}/resources/policy-files/${policy.templateFile}"),
-          "#keyVaultHost#", var.key_vault_host),
-          "#pihHost#", var.pih_host),
-          "#snowHost#", var.snow_host)
-        "display_name" = policy.display_name
-        "method"       = policy.method
-        "url_template" = policy.url_template
-        "description"  = policy.description
-    }
-  ])
+  remap_entries = var.env
 }
 
 data "policy_file" "api_operation_template" {
