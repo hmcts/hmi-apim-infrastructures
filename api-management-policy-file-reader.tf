1acfab7dc275b4c79c15448976fdf2c83eca4cbc
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 11:21:44 +0100

    add policy xml content

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -6,6 +6,10 @@ output "policy_template_text" {
   value = [for policy in local.policies_data.policies :
     {
       operation_id = policy.operationId
+      xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${policy.templateFile}"),
+        "#keyVaultHost#", var.key_vault_host),
+        "#pihHost#", var.pih_host),
+        "#snowHost#", var.snow_host)
       display_name = policy.display_name
       method       = policy.method
       url_template = policy.url_template
