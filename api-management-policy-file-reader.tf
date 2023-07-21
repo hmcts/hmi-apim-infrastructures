0b89cc03bd95e0814aa092195d913be948ce50cc
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-21 10:07:17 +0100

    Test

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -1,21 +1,23 @@
 locals {
   policies_data = jsondecode(file("${path.module}/resources/policy-files/policies.json"))
   policies = [for policy in local.policies_data.policies : policy]
+
+  remap_entries = [
+  for policy in local.policies : {
+
+    operation_id = policy.operationId
+    xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${policy.templateFile}"),
+      "#keyVaultHost#", var.key_vault_host),
+      "#pihHost#", var.pih_host),
+      "#snowHost#", var.snow_host)
+    display_name = policy.display_name
+    method       = policy.method
+    url_template = policy.url_template
+    description  = policy.description
+  }
+  ]
 }
 
 data "policy_file" "api_operation_template" {
-  remap_entries = [
-      for policy in local.policies : {
-
-        operation_id = policy.operationId
-        xml_content  = replace(replace(replace(file("${path.module}/resources/policy-files/${policy.templateFile}"),
-          "#keyVaultHost#", var.key_vault_host),
-          "#pihHost#", var.pih_host),
-          "#snowHost#", var.snow_host)
-        display_name = policy.display_name
-        method       = policy.method
-        url_template = policy.url_template
-        description  = policy.description
-      }
-    ]
+  TO_BE_REPLACED = local.remap_entries
 }
