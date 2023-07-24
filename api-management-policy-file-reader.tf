810fe56214e6feb15688eef59173815eefed55a5
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-24 12:15:23 +0100

    fix bug

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -22,7 +22,7 @@ locals {
       ] : []
       response = can(policy.response) ? {
         status_code = policy.response.status_code
-        description = policy.response.description
+        description = can(policy.response.description) ? can(policy.response.description) : ""
       } : []
       query_parameters =  can(policy.query_parameters) ? [for query_parameter in policy.query_parameters :
       {
@@ -36,7 +36,7 @@ locals {
         name = policy.template_parameter.name
         required = policy.template_parameter.required
         type = policy.template_parameter.type
-        default_value = can(policy.template_parameter.default_value) ? template_parameter.default_value : ""
+        default_value = can(policy.template_parameter.default_value) ? policy.template_parameter.default_value : ""
       } : []
     }
   ]
