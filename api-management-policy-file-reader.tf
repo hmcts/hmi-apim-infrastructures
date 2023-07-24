986814ce22905b49b4f6a20306f82e557e32432e
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-24 12:27:40 +0100

    fixing bug

--- api-management-policy-file-reader.tf
+++ api-management-policy-file-reader.tf
@@ -17,26 +17,26 @@ locals {
           name = header.name
           required = header.required
           type = header.type
-          default_value = can(header.default_value) ? header.default_value : null
+          default_value = can(header.default_value) ? tostring(header.default_value) : ""
         }
       ] : []
       response = can(policy.response) ? {
         status_code = policy.response.status_code
-        description = can(policy.response.description) ? policy.response.description : null
+        description = can(policy.response.description) ? tostring(policy.response.description) : ""
       } : []
       query_parameters =  can(policy.query_parameters) ? [for query_parameter in policy.query_parameters :
       {
         name = query_parameter.name
         required = query_parameter.required
         type = query_parameter.type
-        default_value = can(query_parameter.default_value) ? query_parameter.default_value : null
+        default_value = can(query_parameter.default_value) ? tostring(query_parameter.default_value) : ""
       }
       ] : []
       template_parameter =  can(policy.template_parameter) ? {
         name = policy.template_parameter.name
         required = policy.template_parameter.required
         type = policy.template_parameter.type
-        default_value = can(policy.template_parameter.default_value) ? policy.template_parameter.default_value : null
+        default_value = can(policy.template_parameter.default_value) ? tostring(policy.template_parameter.default_value) : ""
       } : []
     }
   ]
