a31d7b7c25986382e54fd052b54f7d3d00859148
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-08-24 16:00:19 +0100

    migrate VH endpoints

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -4,6 +4,8 @@ locals {
   key_vault_name      = "${local.prefix}-kv-${var.env}"
   pip_client_host = "pip-client-host"
   servicenow_host = "hmi-servicenow-host"
+  vh_client_host = "vh-client-host"
+  vh_OAuth_url = "vh-OAuth-url"
 }
 
 data "azurerm_client_config" "current" {}
@@ -13,14 +15,26 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = local.resource_group_name
 }
 
-data "azurerm_key_vault_secret" "pip_host" {
+data "azurerm_key_vault_secret" "pip_client_host" {
   count        = local.deploy_apim
   name         = local.pip_client_host
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
-data "azurerm_key_vault_secret" "snow_host" {
+data "azurerm_key_vault_secret" "servicenow_host" {
   count        = local.deploy_apim
   name         = local.servicenow_host
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "vh_client_host" {
+  count        = local.deploy_apim
+  name         = local.vh_client_host
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "vh_OAuth_url" {
+  count        = local.deploy_apim
+  name         = local.vh_OAuth_url
   key_vault_id = data.azurerm_key_vault.kv.id
 }
