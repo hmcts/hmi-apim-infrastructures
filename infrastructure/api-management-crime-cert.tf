a91d8a1f7d8faa5cdabeedaa70b586122914cd93
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-22 12:01:53 +0100

    Test change id

--- infrastructure/api-management-crime-cert.tf
+++ infrastructure/api-management-crime-cert.tf
@@ -2,6 +2,6 @@ resource "azurerm_api_management_certificate" "api_management_crime_cert" {
   name                         = "hmi-crime-cert"
   api_management_name          = data.azurerm_api_management.sds_apim.name
   resource_group_name          = local.apim_rg
-  key_vault_secret_id          = data.azurerm_key_vault_certificate.crime_cert.secret_id
+  key_vault_secret_id          = data.azurerm_key_vault_certificate.crime_cert.id
   key_vault_identity_client_id = data.azurerm_user_assigned_identity.hmi.client_id
 }
