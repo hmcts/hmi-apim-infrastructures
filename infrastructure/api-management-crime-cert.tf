d05bf405bdd58b092d33b94a9cfa7d7aad5e2391
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-22 09:09:53 +0100

    Added MI to cert block

--- infrastructure/api-management-crime-cert.tf
+++ infrastructure/api-management-crime-cert.tf
@@ -3,4 +3,5 @@ resource "azurerm_api_management_certificate" "api_management_crime_cert" {
   api_management_name = data.azurerm_api_management.sds_apim.name
   resource_group_name = local.apim_rg
   key_vault_secret_id = data.azurerm_key_vault_certificate.crime_cert.secret_id
+  key_vault_identity_client_id = data.azurerm_user_assigned_identity.hmi.client_id
 }
