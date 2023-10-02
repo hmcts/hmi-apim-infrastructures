c84f85b0c3cfc906ff9424327f2ef295f0137d22
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-10-02 11:33:06 +0100

    Implement functional tests used by CFT/CRIME/SNL

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmi/hearings/PutHearingTest.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.reform.hmi.hearings;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.hmi.helper.HeaderHelper;
+import uk.gov.hmcts.reform.hmi.helper.RestClientHelper;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Functional tests for the endpoint used by different consumers to update hearing (/hearings).
+ */
+@SpringBootTest
+@ActiveProfiles(profiles = "functional")
+class PutHearingTest {
+
+    @Autowired
+    RestClientHelper restClientHelper;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "https://sds-api-mgmt.staging.platform.hmcts.net/hmi";
+    }
+
+    /**
+     * Test with a empty payload for CRIME, a valid set of headers and valid payload, expect 404.
+     */
+    @Test
+    void putHearingCrimeFail() throws IOException {
+        restClientHelper.performSecurePutRequestAndValidate(
+                "{}",
+                HeaderHelper.createHeaders("CRIME"),
+                "/hearings/123",
+                "",
+                404
+        );
+    }
+
+    /**
+     * Test with a empty payload for CFT, a valid set of headers and valid payload, expect 404.
+     */
+    @Test
+    void putHearingCftFail() throws IOException {
+        restClientHelper.performSecurePutRequestAndValidate(
+                "{}",
+                HeaderHelper.createHeaders("CFT"),
+                "/hearings/123",
+                "",
+                404
+        );
+    }
+
+    /**
+     * Test with a Invalid header, response should return 400.
+     */
+    @Test
+    void putHearingInvalidHeaderFail() throws IOException {
+        Map<String, String> requestHeader =  HeaderHelper.createHeaders("CRIME");
+        requestHeader.remove("Destination-System");
+
+        restClientHelper.performSecurePutRequestAndValidate(
+                "{}",
+                requestHeader,
+                "/hearings/123",
+                "Missing/Invalid Header Destination-System",
+                400
+        );
+    }
+
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmi/schedules/PostScheduleTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.hmi.schedules;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.hmi.helper.HeaderHelper;
+import uk.gov.hmcts.reform.hmi.helper.RestClientHelper;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+/**
+ * Functional tests for the endpoint used by different consumers (/schedules).
+ */
+@SpringBootTest
+@ActiveProfiles(profiles = "functional")
+class PostScheduleTest {
+
+    @Autowired
+    RestClientHelper restClientHelper;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "https://sds-api-mgmt.staging.platform.hmcts.net/hmi";
+    }
+
+    /**
+     * Test with a valid set of headers, but empty body, response should return 400.
+     */
+    @Test
+    void postCreateScheduleFail() throws UnknownHostException {
+        restClientHelper.performSecurePostRequestAndValidate(
+                "{}",
+                HeaderHelper.createHeaders("SNL"),
+                "/schedules",
+                "",
+                400
+        );
+    }
+
+    /**
+     * Test with a Invalid header, response should return 400.
+     */
+    @Test
+    void postCreateScheduleHeaderFail() throws IOException {
+        Map<String, String> requestHeader =  HeaderHelper.createHeaders("SNL");
+        requestHeader.remove("Destination-System");
+
+        restClientHelper.performSecurePostRequestAndValidateWithResponse(
+                "{}",
+                requestHeader,
+                "/schedules",
+                "Missing/Invalid Header Destination-System",
+                400
+        );
+    }
+}
