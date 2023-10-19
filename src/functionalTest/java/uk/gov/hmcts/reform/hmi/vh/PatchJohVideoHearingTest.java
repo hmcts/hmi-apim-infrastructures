b680927108a168c2158fa5a567a6a5806bc03894
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-10-19 11:42:35 +0100

    Add Video Hearing functional tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmi/vh/PatchJohVideoHearingTest.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.hmi.vh;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.hmi.helper.HeaderHelper;
+import uk.gov.hmcts.reform.hmi.helper.RestClientHelper;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import static uk.gov.hmcts.reform.hmi.helper.FileHelper.getHearingId;
+import static uk.gov.hmcts.reform.hmi.helper.FileHelper.getJsonPayloadFileAsString;
+
+/**
+ * Functional tests for the VH endpoint (/hearings/{hearingId}/joh/{personalCode}).
+ */
+@SpringBootTest
+@ActiveProfiles(profiles = "functional")
+class PatchJohVideoHearingTest {
+
+    @Autowired
+    RestClientHelper restClientHelper;
+
+    private String validHearingId = "";
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "https://sds-api-mgmt.staging.platform.hmcts.net/hmi";
+    }
+
+    /**
+     * Test with a valid hearing id and a valid set of headers, response will be 404
+     * because we do not have judge id assigned on VH side.
+     */
+    @Test
+    void vhPatchJohVideoHearingFailed() throws IOException {
+        Response response = restClientHelper.performSecurePostRequestAndValidateWithResponse(
+                getJsonPayloadFileAsString("vh/create-vh-hearing.json"),
+                HeaderHelper.createHeaders("VH"),
+                "/resources/video-hearing",
+                "id",
+                201
+        );
+
+        validHearingId = getHearingId(response);
+
+        restClientHelper.performSecurePatchRequestAndValidate(
+                getJsonPayloadFileAsString("vh/update-joh-video-hearing.json"),
+                HeaderHelper.createHeaders("VH"),
+                String.format("/hearings/%s/joh/%s", validHearingId, "123"),
+                "",
+                404
+        );
+    }
+
+    /**
+     * Test with an invalid hearing id and a valid set of headers, response should return 404.
+     */
+    @Test
+    void vhPatchJohVideoHearingInvalidId() throws UnknownHostException {
+        restClientHelper.performSecurePatchRequestAndValidate(
+                "{}",
+                HeaderHelper.createHeaders("VH"),
+                String.format("/hearings/%s/joh/invalid", validHearingId),
+                "",
+                404
+        );
+    }
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmi/vh/PostJohVideoHearingTest.java
@@ -0,0 +1,70 @@
+package uk.gov.hmcts.reform.hmi.vh;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.hmi.helper.HeaderHelper;
+import uk.gov.hmcts.reform.hmi.helper.RestClientHelper;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import static uk.gov.hmcts.reform.hmi.helper.FileHelper.getHearingId;
+import static uk.gov.hmcts.reform.hmi.helper.FileHelper.getJsonPayloadFileAsString;
+
+/**
+ * Functional tests for the VH endpoint (/hearings/{hearingId}/joh).
+ */
+@SpringBootTest
+@ActiveProfiles(profiles = "functional")
+class PostJohVideoHearingTest {
+
+    @Autowired
+    RestClientHelper restClientHelper;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "https://sds-api-mgmt.staging.platform.hmcts.net/hmi";
+    }
+
+    /**
+     * Test with a valid hearing id and a valid set of headers, response will be 404
+     * because we do not have judge id assigned on VH side.
+     */
+    @Test
+    void vhPostAddJohVideoHearingFailed() throws IOException {
+        Response response = restClientHelper.performSecurePostRequestAndValidateWithResponse(
+                getJsonPayloadFileAsString("vh/create-vh-hearing.json"),
+                HeaderHelper.createHeaders("VH"),
+                "/resources/video-hearing",
+                "id",
+                201
+        );
+
+        restClientHelper.performSecurePostRequestAndValidate(
+                getJsonPayloadFileAsString("vh/create-joh-video-hearing.json"),
+                HeaderHelper.createHeaders("VH"),
+                String.format("/hearings/%s/joh", getHearingId(response)),
+                "",
+                404
+        );
+    }
+
+    /**
+     * Test with an invalid hearing id and a valid set of headers, response should return 400.
+     */
+    @Test
+    void vhPostAddJohVideoHearingInvalidId() throws UnknownHostException {
+        restClientHelper.performSecurePostRequestAndValidate(
+                "{}",
+                HeaderHelper.createHeaders("VH"),
+                "/hearings/invalid/joh",
+                "",
+                400
+        );
+    }
+}
