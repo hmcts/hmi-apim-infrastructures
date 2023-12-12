a6fcbdf6eaa2434345c31c851048c90551935eb2
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-12-12 14:26:52 +0000

    permission to sds apim

---
+++ infrastructure/api-management-logger-permission.tf
@@ -0,0 +1,9 @@
+resource "azurerm_role_assignment" "app_insight_viewers" {
+  scope                = azurerm_api_management_api_diagnostic.api_logs.api_management_logger_id
+  role_definition_name = "Application Insights Component Contributor"
+  principal_id         = data.azuread_group.hmi-group.id
+}
+
+data "azuread_group" "hmi-group" {
+  display_name     = "DTS HMI App Insights"
+}
