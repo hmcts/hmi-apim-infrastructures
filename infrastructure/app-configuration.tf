37046daa9d45c8ab1f83ff5e9351c94339465b08
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-01-02 13:29:10 +0000

    fixing build

--- infrastructure/app-configuration.tf
+++ infrastructure/app-configuration.tf
@@ -13,7 +13,7 @@ data "azurerm_client_config" "current_app_config" {}
 resource "azurerm_role_assignment" "app_conf_data_owner" {
   scope                = azurerm_app_configuration.app_conf.id
   role_definition_name = "App Configuration Data Owner"
-  principal_id         = data.azurerm_client_config.current_app_config.object_id
+  principal_id         = data.azurerm_client_config.current_app_config.client_id
 }
 
 resource "azurerm_app_configuration_feature" "test" {
