50fc3ba1a1db9bdf8c2c5f43a678c9d3673acb46
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-01-02 12:19:39 +0000

    add test feature flag

--- infrastructure/app-configuration.tf
+++ infrastructure/app-configuration.tf
@@ -7,3 +7,19 @@ resource "azurerm_app_configuration" "app_conf" {
   resource_group_name = local.api_resource_group
   location            = var.location
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "appconf_dataowner" {
+  scope                = azurerm_app_configuration.app_conf.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_app_configuration_feature" "test" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  description            = "test description"
+  name                   = "test-ackey"
+  label                  = "test-ackeylabel"
+  enabled                = true
+}
