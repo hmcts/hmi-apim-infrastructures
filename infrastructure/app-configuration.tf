45d6ea4990f6d10395faddb85ff720b4a0d52a27
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-01-02 16:38:16 +0000

    fixing issue

--- infrastructure/app-configuration.tf
+++ infrastructure/app-configuration.tf
@@ -2,16 +2,23 @@ locals {
   app_config_name = "${var.product}-app-config-${var.env}"
 }
 
-resource "azurerm_app_configuration" "app_conf" {
-  name                = local.app_config_name
-  resource_group_name = local.api_resource_group
-  location            = var.location
+data "azurerm_client_config" "test" {
 }
 
 resource "azurerm_role_assignment" "app_conf_data_owner" {
   scope                = azurerm_app_configuration.app_conf.id
   role_definition_name = "App Configuration Data Owner"
-  principal_id         = "ded9b69f-b5b0-420e-8a02-fd97e6d04d8d"
+  principal_id         = data.azurerm_client_config.test.object_id
+}
+
+resource "azurerm_app_configuration" "app_conf" {
+  name                = local.app_config_name
+  resource_group_name = local.api_resource_group
+  location            = var.location
+
+  depends_on = [
+    azurerm_role_assignment.app_conf_data_owner,
+  ]
 }
 
 resource "azurerm_app_configuration_feature" "test" {
