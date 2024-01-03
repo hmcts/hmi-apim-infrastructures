9e78fce7481eca28366dff153313b35727717458
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-01-03 11:46:49 +0000

    fixing build

--- infrastructure/app-configuration.tf
+++ infrastructure/app-configuration.tf
@@ -2,19 +2,32 @@ locals {
   app_config_name = "${var.product}-app-config-${var.env}"
 }
 
-resource "azurerm_app_configuration" "app_conf" {
-  name                = local.app_config_name
-  resource_group_name = local.api_resource_group
-  location            = var.location
+resource "azurerm_resource_group" "rg" {
+  name     = local.api_resource_group
+  location = var.location
+}
+
+data "azurerm_client_config" "test" {
 }
 
 resource "azurerm_role_assignment" "app_conf_data_owner" {
-  scope                = azurerm_app_configuration.app_conf.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "App Configuration Data Owner"
-  principal_id         = "ded9b69f-b5b0-420e-8a02-fd97e6d04d8d"
+  principal_id         = data.azurerm_client_config.test.object_id
 }
 
-resource "azurerm_app_configuration_feature" "test" {
+resource "azurerm_app_configuration" "app_conf" {
+  name                = local.app_config_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  depends_on = [
+    azurerm_role_assignment.app_conf_data_owner,
+  ]
+}
+
+resource "azurerm_app_configuration_feature" "test_feature" {
   configuration_store_id = azurerm_app_configuration.app_conf.id
   description            = "test description"
   name                   = "test-ackey"
