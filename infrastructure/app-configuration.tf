4dd6cbc0c81d687fc5600411f376ed8272431c93
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-01-04 10:47:04 +0000

    fixing build

--- infrastructure/app-configuration.tf
+++ infrastructure/app-configuration.tf
@@ -2,24 +2,19 @@ locals {
   app_config_name = "${var.product}-app-config-${var.env}"
 }
 
-data "azurerm_client_config" "test" {
-}
-
-resource "azurerm_role_assignment" "app_conf_data_owner" {
-  scope                = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/ss-sbox-network-rg"
-  role_definition_name = "App Configuration Data Owner"
-  principal_id         = data.azurerm_client_config.test.object_id
-}
-
 resource "azurerm_app_configuration" "app_conf" {
   name                = local.app_config_name
   resource_group_name = local.api_resource_group
   location            = var.location
   sku                 = "standard"
+}
 
-  depends_on = [
-    azurerm_role_assignment.app_conf_data_owner,
-  ]
+data "azurerm_client_config" "test" {}
+
+resource "azurerm_role_assignment" "app_conf_data_owner" {
+  scope                = azurerm_app_configuration.app_conf.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = data.azurerm_client_config.test.object_id
 }
 
 resource "azurerm_app_configuration_feature" "test_feature" {
