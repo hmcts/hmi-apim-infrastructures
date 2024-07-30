c84032a252adc7ad0dc052c301bdedbd6d27bb45
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-07-30 16:27:00 +0100

    PUB-2387 - Fix showing of diff

--- infrastructure/api-management-product.tf
+++ infrastructure/api-management-product.tf
@@ -5,6 +5,8 @@ module "api_mgmt_product" {
   subscription_required = "false"
   api_mgmt_name         = local.apim_name
   api_mgmt_rg           = local.api_resource_group
+  //Setting this to 0 prevents terraform flagging this as a change each time, as the default is 20 which never gets set due to the subscription_required being false
+  subscriptions_limit   = "0"
   product_policy = replace(replace(file("${path.module}/resources/policy-files/product-policy.xml"),
     "#keyVaultHost#", var.key_vault_host),
   "#rateCallLimit#", var.rate-call-limit)
