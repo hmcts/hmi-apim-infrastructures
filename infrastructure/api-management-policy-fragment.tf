ede5ba22900b990e86a4ab9ec8a753ede3f6b852
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-11-28 17:19:34 +0000

    test

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -2,5 +2,6 @@ resource "azurerm_api_management_policy_fragment" "example" {
   api_management_id = data.azurerm_api_management.sds_apim.id
   name              = "${var.product}-product-policy-fragment"
   format            = "rawxml"
-  value             = "<fragment><set-method>GET</set-method></fragment>"
+  value             = replace(file("${path.module}/resources/policy-fragments/generate-snl-auth-token.xml"),
+    "#keyVaultHost#", var.key_vault_host)
 }
