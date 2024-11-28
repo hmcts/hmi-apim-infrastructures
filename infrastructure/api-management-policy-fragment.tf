e23aeef4543f13d4fa297a0c41ea32bfcaddc1d8
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-11-28 16:15:24 +0000

    WIP

---
+++ infrastructure/api-management-policy-fragment.tf
@@ -0,0 +1,7 @@
+resource "azurerm_api_management_policy_fragment" "example" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-product-policy-fragment"
+  format            = "xml"
+  value             = replace(file("${path.module}/resources/policy-fragments/generate-snl-auth-token.xml"),
+                        "#keyVaultHost#", var.key_vault_host)
+}
