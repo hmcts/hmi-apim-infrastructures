4d0bf5dd98acf98f06a275c65397de5e5da3c74a
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-11-29 09:35:25 +0000

    add snl oauth url

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -3,6 +3,7 @@ resource "azurerm_api_management_policy_fragment" "example" {
   name              = "${var.product}-snl-token-generation-policy-fragment"
   format            = "rawxml"
   description       = "This fragment contains code to generate the snl auth token"
-  value             = replace(file("${path.module}/resources/policy-fragments/generate-snl-auth-token.xml"),
-    "#keyVaultHost#", var.key_vault_host)
+  value             = replace(replace(file("${path.module}/resources/policy-fragments/generate-snl-auth-token.xml"),
+    "#keyVaultHost#", var.key_vault_host),
+    "#sAndLOauthUrl#", length(data.azurerm_key_vault_secret.snl_OAuth_url) > 0 ? data.azurerm_key_vault_secret.snl_OAuth_url[0].value : "")
 }
