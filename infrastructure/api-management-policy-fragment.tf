c0661a417be82d7ff3c0951896ffe43ea33d85ee
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-11-29 15:42:13 +0000

    destination authentication failed

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -23,3 +23,11 @@ resource "azurerm_api_management_policy_fragment" "invalid_destination_response"
   description       = "This fragment contains code which send error response if request has invalid destination in header"
   value             = file("${path.module}/resources/policy-fragments/invalid-destination-response-fragment.xml")
 }
+
+resource "azurerm_api_management_policy_fragment" "failed_destination_authentication_response" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-failed-destination-authentication-response"
+  format            = "rawxml"
+  description       = "This fragment contains code which send error response if destination fails to authenticate HMI request"
+  value             = file("${path.module}/resources/policy-fragments/failed-destination-authentication-response-fragment.xml")
+}
