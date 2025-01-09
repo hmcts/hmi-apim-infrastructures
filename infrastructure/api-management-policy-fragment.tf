217d39192384adf1467b8b2576255905bcd97527
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2025-01-09 09:13:14 +0000

    fix bug

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -5,15 +5,7 @@ resource "azurerm_api_management_policy_fragment" "vh_auth_token_generation" {
   description       = "This fragment contains code to generate the authentication token to communicate with Video Hearing"
   value = replace(replace(file("${path.module}/resources/policy-fragments/vh-auth-token-generation-fragment.xml"),
     "#keyVaultHost#", var.key_vault_host),
-  "#vhOauthUrl#", length(data.azurerm_key_vault_secret.vh_OAuth_url) > 0 ? data.azurerm_key_vault_secret.vh_OAuth_url[0].value : "")
-}
-
-resource "azurerm_api_management_policy_fragment" "mock_generic_response" {
-  api_management_id = data.azurerm_api_management.sds_apim.id
-  name              = "${var.product}-mock-generic-response"
-  format            = "rawxml"
-  description       = "This fragment contains code which generate generic response for MOCK"
-  value             = file("${path.module}/resources/policy-fragments/mock-generic-response-fragment.xml")
+    "#vhOauthUrl#", length(data.azurerm_key_vault_secret.vh_OAuth_url) > 0 ? data.azurerm_key_vault_secret.vh_OAuth_url[0].value : "")
 }
 
 resource "azurerm_api_management_policy_fragment" "invalid_destination_response" {
