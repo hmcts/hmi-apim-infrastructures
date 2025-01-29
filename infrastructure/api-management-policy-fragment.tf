98e9b980b596e55ce9d5ed2b478b1c446e65e807
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-01-29 16:30:21 +0000

    Add policy fragments

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -41,6 +41,30 @@ resource "azurerm_api_management_policy_fragment" "invalid_query_parameters_resp
   value             = file("${path.module}/resources/policy-fragments/invalid-query-parameters-response-fragment.xml")
 }
 
+resource "azurerm_api_management_policy_fragment" "missing_mandatory_updated_since_response" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-missing-mandatory-updated-since-response"
+  format            = "rawxml"
+  description       = "This fragment contains code which send error response if request contains invalid query parameters"
+  value             = file("${path.module}/resources/policy-fragments/missing-mandatory-updated-since-response-fragment.xml")
+}
+
+resource "azurerm_api_management_policy_fragment" "elinks_mock_person_response" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-elinks-mock-person-response"
+  format            = "rawxml"
+  description       = "This fragment contains code which send error response if request contains invalid query parameters"
+  value             = file("${path.module}/resources/policy-fragments/elinks-mock-person-response-fragment.xml")
+}
+
+resource "azurerm_api_management_policy_fragment" "elinks_mock_multi_person_response" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-elinks-mock-multi-person-response"
+  format            = "rawxml"
+  description       = "This fragment contains code which send error response if request contains invalid query parameters"
+  value             = file("${path.module}/resources/policy-fragments/elinks-mock-multi-person-response-fragment.xml")
+}
+
 resource "azurerm_api_management_policy_fragment" "elinks_auth_token_generation" {
   api_management_id = data.azurerm_api_management.sds_apim.id
   name              = "${var.product}-elinks-auth-token-generation"
