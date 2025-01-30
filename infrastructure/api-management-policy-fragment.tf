6713f6d6a108b547da0bbc6692802e013cfc16e4
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-01-30 13:47:21 +0000

    Fix typo

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -72,5 +72,5 @@ resource "azurerm_api_management_policy_fragment" "elinks_auth_token_generation"
   description       = "This fragment contains code to generate the authentication token to communicate with eLinks"
   value = replace(replace(file("${path.module}/resources/policy-fragments/elinks-auth-token-generation-fragment.xml"),
     "#keyVaultHost#", var.key_vault_host),
-    "#elinksHost#", length(data.azurerm_key_vault_secret.elinks_client_host) > 0 ? data.azurerm_key_vault_secret.elinks_client_host[0].value : ""),
+    "#elinksHost#", length(data.azurerm_key_vault_secret.elinks_client_host) > 0 ? data.azurerm_key_vault_secret.elinks_client_host[0].value : "")
 }
