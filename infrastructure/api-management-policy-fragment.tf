2c1d8c043cdca083627ddfbc78bbe807bbb432b0
kian.kwa <kian.kwa@justice.gov.uk>
2025-09-19 12:12:56 +0100

    Fixed terraform resource value

--- infrastructure/api-management-policy-fragment.tf
+++ infrastructure/api-management-policy-fragment.tf
@@ -46,7 +46,7 @@ resource "azurerm_api_management_policy_fragment" "snl-auth-token-generation" {
   name              = "${var.product}-snl-auth-token-generation"
   format            = "rawxml"
   description       = "This fragment contains code to generate the authentication token to communicate with SNL"
-  value = replace(replace(file("${path.module}/resources/policy-fragments/snl-headers-fragment.xml"),
+  value = replace(replace(file("${path.module}/resources/policy-fragments/snl-auth-token-generation-fragment.xml"),
   "#keyVaultHost#", var.key_vault_host),
   "#sAndLOauthUrl#", length(data.azurerm_key_vault_secret.snl_OAuth_url) > 0 ? data.azurerm_key_vault_secret.snl_OAuth_url[0].value : "")
 }
