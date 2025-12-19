9453ebec0f823efec8bbe813cba8b7fedd8149db
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-19 10:10:19 +0000

    Remove Crime endpoints from HMI

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -5,12 +5,9 @@ locals {
   pip_client_host     = "pip-client-host"
   cft_client_host     = "cft-client-host"
   cft_OAuth_url       = "cft-OAuth-url"
-  crime_client_host   = "crime-client-host"
   elinks_client_host  = "elinks-client-host"
   snl_client_host     = "snl-client-host"
   snl_OAuth_url       = "snl-OAuth-url"
-  crime_cert_base     = "hmi-crime-cert-base-64"
-  crime_cert_password = "hmi-crime-cert-password"
 }
 
 data "azurerm_client_config" "current" {}
@@ -38,12 +35,6 @@ data "azurerm_key_vault_secret" "cft_OAuth_url" {
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
-data "azurerm_key_vault_secret" "crime_client_host" {
-  count        = local.deploy_apim
-  name         = local.crime_client_host
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
-
 data "azurerm_key_vault_secret" "elinks_client_host" {
   count        = local.deploy_apim
   name         = local.elinks_client_host
@@ -59,16 +50,6 @@ data "azurerm_key_vault_secret" "snl_client_host" {
 data "azurerm_key_vault_secret" "snl_OAuth_url" {
   count        = local.deploy_apim
   name         = local.snl_OAuth_url
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
-
-data "azurerm_key_vault_secret" "crime_cert_base" {
-  name         = local.crime_cert_base
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
-
-data "azurerm_key_vault_secret" "crime_cert_password" {
-  name         = local.crime_cert_password
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
