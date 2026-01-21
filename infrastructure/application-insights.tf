093e822347b42be9b52432c439f2a799dfd5d760
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-01-21 15:36:54 +0000

    test

--- infrastructure/application-insights.tf
+++ infrastructure/application-insights.tf
@@ -13,3 +13,14 @@ moved {
   from = azurerm_application_insights.appinsights
   to   = module.application_insights.azurerm_application_insights.this
 }
+
+# You should have something like this in your Terraform:
+resource "azurerm_api_management_logger" "appinsights" {
+  name                = "sds-api-mgmt-${local.env}-logger"
+  api_management_name = local.apim_name
+  resource_group_name = local.apim_rg
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.this.instrumentation_key
+  }
+}
