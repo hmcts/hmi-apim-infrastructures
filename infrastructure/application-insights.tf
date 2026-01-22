5f154c8e4d766ac432d10f623b4ad5765d5204b9
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-01-22 09:18:01 +0000

    Automated Change

--- infrastructure/application-insights.tf
+++ infrastructure/application-insights.tf
@@ -2,20 +2,20 @@ module "application_insights" {
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env     = var.env
-  name   = "${var.product}-${local.apim_name}"
+  name    = "${var.product}-${local.apim_name}"
   product = var.product
 
   resource_group_name = local.apim_rg
   sampling_percentage = var.sampling_percentage
   common_tags         = var.common_tags
-  count = local.env == "prod" ? 0 : 1
+  count               = local.env == "prod" ? 0 : 1
 }
 
 resource "azurerm_api_management_logger" "ai-logger" {
   name                = "${var.product}-${local.apim_name}-logger"
   api_management_name = local.apim_name
   resource_group_name = local.resource_group_name
-  count = local.env == "prod" ? 0 : 1
+  count               = local.env == "prod" ? 0 : 1
 
   application_insights {
     connection_string = module.application_insights.connection_string
