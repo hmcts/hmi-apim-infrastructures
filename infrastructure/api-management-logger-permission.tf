e8f5990cb3b1db84cfbd8ed9a21953f6bb78fa3d
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-01-22 15:26:03 +0000

    fix permission issue

--- infrastructure/api-management-logger-permission.tf
+++ infrastructure/api-management-logger-permission.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "app_insight_viewers" {
-  scope                = azurerm_api_management_api_diagnostic.api_logs[0].api_management_logger_id
+  scope                = module.application_insights[0].id
   role_definition_name = "Application Insights Component Contributor"
   principal_id         = data.azuread_group.hmi-group.object_id
   count                = local.env == "prod" ? 0 : 1
