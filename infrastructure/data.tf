ceafa14409abe1e592749191fb07b2d4b60e97a2
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-01-22 09:57:46 +0000

    Automated Change

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -8,7 +8,7 @@ locals {
   elinks_client_host  = "elinks-client-host"
   snl_client_host     = "snl-client-host"
   snl_OAuth_url       = "snl-OAuth-url"
-  apim_resource_name = "ss-${var.env}-network-rg"
+  apim_resource_name  = "ss-${var.env}-network-rg"
 }
 
 data "azurerm_client_config" "current" {}
